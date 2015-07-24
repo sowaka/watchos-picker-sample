30733c215fdb7adf32da9eefc43b5b8587e1d3e1
Sowaka <sowaka@eventopia.jp>
2015-07-24 10:06:09 +0900

    Initial Commit

---
+++ PickerSample WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  PickerSample WatchKit Extension
+//
+//  Created by Sowaka on 2015/07/24.
+//  Copyright © 2015年 com.myCompany. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
